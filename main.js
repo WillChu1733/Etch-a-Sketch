6e722d0b26eb75aa3e7af20f3f7cc01d481ebc0a
William Chu <willchu1733@gmail.com>
2024-05-30 11:43:37 +1200

    add button event listener

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // selecting the first element with the class 'container', assigning it to variable container
 const container = document.querySelector('.container')
 const num = 16
+const resetButton = document.querySelector('.reset-button')
 
 const createDiv = () => {
   for (let i = 0; i < num; i++) {
@@ -10,8 +11,8 @@ const createDiv = () => {
     for (let j = 0; j < num; j++) {
       const gridBox = document.createElement('div')
       gridBox.classList.add('grid-box')
-      // grid-row becomes parent of grid-box
       gridBox.addEventListener('mouseenter', mouseEnter)
+      // grid-row becomes parent of grid-box
       gridRow.appendChild(gridBox)
     }
     container.appendChild(gridRow)
@@ -21,4 +22,14 @@ const createDiv = () => {
 function mouseEnter(event) {
   event.target.style.backgroundColor = 'black'
 }
+
+resetButton.addEventListener('click', () => {
+  let userSize = Number(prompt('What Dimensions'))
+
+  while (userSize > 100) {
+    userSize = Number(prompt('What Dimensions, less than 100'))
+  }
+  createDiv(userSize)
+})
+
 createDiv()
